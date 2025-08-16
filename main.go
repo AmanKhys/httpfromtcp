eb749fe9313c49215e823020556287a0e1e73dc2
AmanKhys <amankhys2112@gmail.com>
2025-08-17 03:52:08 +0530

    read as 8 bytes from stream; stdout when it encounters '\n' in the format read: LINE potential problems:- ~ io.EOF reaches and some of the 8 bytes were written into txt and not printed out ~ out of array index problem with adding currentline = string(txt[k+1:])

--- main.go
+++ main.go
@@ -11,12 +11,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	var currentLine string
 	for {
 		txt := make([]byte, 8)
 		_, err = msgFile.Read(txt)
 		if err == io.EOF {
 			break
 		}
-		fmt.Printf("read: %s\n", txt)
+		ok, k := checkAndFindIfNewLineInText(txt)
+		if !ok {
+			currentLine = currentLine + string(txt)
+		} else {
+			currentLine = currentLine + string(txt[:k])
+			fmt.Printf("read: %s\n", currentLine)
+			currentLine = string(txt[k+1:])
+		}
 	}
+}
+
+func checkAndFindIfNewLineInText(txt []byte) (bool, int) {
+	var i int
+	for ; i < len(txt); i++ {
+		if string(txt[i]) == fmt.Sprintf("\n") {
+			return true, i
+		}
+	}
+	return false, -1
 }
