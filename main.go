9ba67e988fcb9afe39b0e17e71e2fd4c76a42460
AmanKhys <amankhys2112@gmail.com>
2025-08-17 20:38:46 +0530

    take the stream of data from a channel which is fed with data in a go routine.

--- main.go
+++ main.go
@@ -11,22 +11,35 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	var currentLine string
-	for {
-		txt := make([]byte, 8)
-		_, err = msgFile.Read(txt)
-		if err == io.EOF {
-			break
-		}
-		ok, k := checkAndFindIfNewLineInText(txt)
-		if !ok {
-			currentLine = currentLine + string(txt)
-		} else {
-			currentLine = currentLine + string(txt[:k])
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = string(txt[k+1:])
-		}
+	messagesChan := getLinesChannel(msgFile)
+	for msg := range messagesChan {
+		fmt.Printf("read: %s\n", msg)
 	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	var currentLine string
+	var stream = make(chan string)
+	go func() {
+		for {
+			txt := make([]byte, 8)
+			_, err := f.Read(txt)
+			if err == io.EOF {
+				close(stream)
+				break
+			}
+			ok, k := checkAndFindIfNewLineInText(txt)
+			if !ok {
+				currentLine = currentLine + string(txt)
+			} else {
+				currentLine = currentLine + string(txt[:k])
+				stream <- currentLine
+				currentLine = string(txt[k+1:])
+			}
+		}
+	}()
+	return stream
 }
 
 func checkAndFindIfNewLineInText(txt []byte) (bool, int) {
